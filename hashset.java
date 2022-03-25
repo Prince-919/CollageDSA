fa366104a5a03669e16118463b8ed00326dc059f
Prince Sharma <princesharmabbu0@gmail.com>
2022-03-25 00:19:54 -0700

    Create Set Implement HashSet

---
+++ hashset.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class hashset {
+    public static void main(String[] args) {
+        
+        Set<Integer> s = new HashSet<>();
+        // Adding 
+        s.add(10);
+        s.add(20);
+        s.add(30);
+        s.add(40);   // [20,40,10,30]
+        
+        //remove
+        //s.remove(40); // [20, 10,30]
+
+        // clear
+       // s.clear();  // []
+
+        int d = s.size();
+
+        System.out.println(d);
+
+        System.out.println(s.contains(70));
+    }
+}
